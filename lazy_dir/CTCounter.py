5f79e9ee26e5237633b73a064557cdb09ee779ec
Kristopher <Kristopherlouis@gmail.com>
2025-11-17 10:38:40 -0800

    multi-class confusion matrices added in lazy_data

---
+++ lazy_dir/CTCounter.py
@@ -0,0 +1,66 @@
+import os
+import copy
+import pandas as pd
+
+class CTCounter():
+    EXTENDED_TABLES_DIRECTORY = os.path.join(
+        '..', 'Data', 'Preprocessed', 'Extended_Tables'
+    )
+    CONSENSUS_TABLES_DIRECTORY = os.path.join(
+        '..', 'Data', 'Consensus', 'Tool_Consensus', 'Mode_Based', 'WithoutR3DMA'
+    )
+    
+    @staticmethod
+    def run() -> None:
+        TOOLS = ['CL', 'FR', 'MC', 'RV', 'DSSR']
+        CONTACT_TYPES = [
+            'cWW','cWH','cWS','cHW','cHH','cHS','cSW','cSH','cSS',
+            'tWW','tWH','tWS','tHW','tHH','tHS','tSW','tSH','tSS','nbp', 'REJECT', '?'
+        ]
+        row_template = {ct: 0 for ct in CONTACT_TYPES}
+        tables_ct = {
+        tool: {row: row_template.copy() for row in CONTACT_TYPES} for tool in TOOLS
+        }
+        
+        # Now convert the above into a dictionary of dataframes
+        for tool in tables_ct.keys():
+            tables_ct[tool] = pd.DataFrame(tables_ct[tool])
+        tables_bp = copy.deepcopy(tables_ct)
+        
+        for consensus_file in os.listdir(CTCounter.CONSENSUS_TABLES_DIRECTORY):
+            consensus = pd.read_csv(os.path.join(CTCounter.CONSENSUS_TABLES_DIRECTORY, consensus_file))
+            extended = pd.read_csv(os.path.join(CTCounter.EXTENDED_TABLES_DIRECTORY, consensus_file))
+            assert len(consensus) == len(extended)
+            CTCounter._compare_tables(consensus, extended, tables_bp, tables_ct)
+        
+        CTCounter._write_tables(tables_ct)
+            
+    @staticmethod
+    def _compare_tables(consensus: pd.DataFrame, extended: pd.DataFrame, tables_bp: dict[pd.DataFrame], tables_ct: dict[pd.DataFrame]) -> None:
+        for column in consensus.columns[::-1]:
+            if '-' not in column:
+                break
+            for c_entry, e_entry in zip(consensus[column], extended[column]):
+                CTCounter._update_count(tables_bp, tables_ct, c_entry, e_entry)
+    
+    @staticmethod
+    def _update_count(tables_bp: dict[pd.DataFrame], tables_ct: dict[pd.DataFrame], c_entry: str, e_entry: str) -> None:
+        if c_entry == 'REJECT':
+            return
+        
+        e_entry = e_entry.split(',')[1:]
+        index_to_tool = {0: 'CL', 1: 'FR', 2: 'MC', 3: 'RV', 4: 'DSSR'}
+        for i in range(len(e_entry)):
+            tool = index_to_tool[i]
+            tool_contact_type = e_entry[i]
+            if c_entry == 'unresolved tie':
+                c_entry = "?"
+            tables_ct[tool].loc[c_entry, tool_contact_type] += 1
+            
+    @staticmethod
+    def _write_tables(tables_ct: dict[pd.DataFrame]) -> None:
+        for tool, df in tables_ct.items():
+            path = os.path.join('lazy_data', f'{tool}_confusion_matrix.csv')
+            df.to_csv(path)
+    
+CTCounter.run()
